65503875008371e72416191df14ba3a3c44ae4a9
Louis Lin <louislin9603@gmail.com>
2024-03-29 14:05:28 -0400

    Test_Phase v3
    Added enemy
    -Idle AI going left and right
    -Changes direction when touching a wall or ledge when idle
    -Chase mechanic when player walks into range
    -Enemy color changes to detect state
    -Started on attacking mechanic

--- Player.gd
+++ Player.gd
@@ -1,73 +1,41 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+# Physics
+const SPEED = 200.0
+const JUMP_VELOCITY = -450.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Variables for double jump
+# Double Jump
 var jump_count = 0
 var max_jumps = 1
-
-# Variables for wall slide
-const WALL_SLIDE_ACCELERATION = 10
-const MAX_WALL_SLIDE_SPEED = 80
 
 # Coyote Timer
 @onready var coyote_timer = $CoyoteJump
 
-# Variables for dashing
-var dash_direction = Vector2(1,0)
-var canDash = false
-var dashing = false
+# Attacking
+var attacking = false
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	
-	dash()
-	
-	if dashing:
-		velocity = velocity.move_toward(Vector2.ZERO, 2000 * delta)
-		move_and_slide()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Get the input direction and handle the movement/deceleration.
+	
+	# Get direction of user input.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jumps:
-		velocity.y = JUMP_VELOCITY
-		jump_count += 1
-		
-		# Jump while on wall
-		if is_on_wall() and Input.is_action_pressed("ui_right"):
-			velocity.y = JUMP_VELOCITY
-			velocity.x = direction * -SPEED
-			
-		elif is_on_wall() and Input.is_action_pressed("ui_left"):
-			velocity.y = JUMP_VELOCITY
-			velocity.x = direction * SPEED
-			
-		
-	# Wall slide
-	if is_on_wall() and (Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left")):
-		if velocity.y >= 0:
-			velocity.y = min(velocity.y + WALL_SLIDE_ACCELERATION, MAX_WALL_SLIDE_SPEED)
-		jump_count = 0	
-	else: 
-		velocity.y += gravity/4 * delta
-			
-	#Reset jump count after falling down
-	if is_on_floor() or coyote_timer.time_left > 0.0:
-		jump_count = 0
-		
+	# Move based on direction (value 1 for right, -1 for left)
 	if direction:
 		velocity.x = direction * SPEED
-		dash()
-	else:
+	else: # Idle
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Stuff for jumping
+	jump()
+	
+	# Stuff for attacking
+	if Input.is_action_just_pressed("ui_attack"):
+		attack()
 	
 	# Coyote Timer - Checks if player is leaving ledge and about to jump
 	var was_on_floor = is_on_floor()
@@ -76,18 +44,23 @@ func _physics_process(delta):
 	if just_left_ledge:
 		coyote_timer.start()
 
-func dash():
-	if is_on_floor():
-		canDash = true
+func jump():
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_select") and jump_count < max_jumps:
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+
+	#Reset jump count after falling down
+	if is_on_floor() or coyote_timer.time_left > 0.0:
+		jump_count = 0	
+
+func attack():
 	
-	if Input.is_action_pressed("ui_right"):
-		dash_direction = Vector2.RIGHT
-	if Input.is_action_pressed("ui_left"):
-		dash_direction = Vector2.LEFT
-		
-	if Input.is_action_just_pressed("ui_dash") and canDash:
-		velocity = dash_direction.normalized() * 2000
-		canDash = false
-		dashing = true
-		await get_tree().create_timer(0.3).timeout
-		dashing = false
+	var hit_list = $AttackArea.get_overlapping_bodies()
+	
+	for area in hit_list:
+		var parent = area.get_parent()
+		print(parent)
+	attacking = true
+	
+	
